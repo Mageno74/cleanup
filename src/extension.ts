88688b585cd96f851708104a264179799f9420c0
Mageno74 <gerhard.mayer2@web.de>
2024-08-23 11:46:22 +0200

    extension vereinfacht

--- src/extension.ts
+++ src/extension.ts
@@ -8,68 +8,37 @@ import { isIBNArc } from './fileTypeTest';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    // NC Programm formatieren und nummerieren
     let cleanupDisposable = vscode.commands.registerCommand('nc_nummerieren', () => {
-
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No active editor
-        }
-        const doc: vscode.TextDocument = editor.document;
-
-        if (isIBNArc(doc)) {
-            vscode.window.showErrorMessage('Fehler >> siehe Menü -> Anzeigen -> Probleme');
-            return;
-        }
-
-        // konrolle IF/ELSE/ENDIF/LOOP/FOR/WHILE
-        let faultArray: Array<any> = openClose(doc);
-
-        const faultTrans: Array<any> = openCloseTrans(doc);
-
-        faultArray = faultArray.concat(faultTrans);
-
-        // falls Fehler gefunden werden --> Fehler anzeigen und Programm abberchen
-        if (markFaults(faultArray, doc)) {
-            vscode.window.showErrorMessage('Fehler >> siehe Menü -> Anzeigen -> Probleme');
-            return;
-        }
-        renumber(doc, editor);
+        processDocument(renumber);
     });
 
-    // NC Programm formatieren
     let onlyFormatDisposable = vscode.commands.registerCommand('nc_formatieren', () => {
-
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No active editor
-        }
-        const doc: vscode.TextDocument = editor.document;
-
-        if (isIBNArc(doc)) {
-            vscode.window.showErrorMessage('Fehler >> siehe Menü -> Anzeigen -> Probleme');
-            return;
-        }
-
-        // konrolle IF/ELSE/ENDIF/LOOP/FOR/WHILE
-        let faultArray: Array<any> = openClose(doc);
-
-        const faultTrans: Array<any> = openCloseTrans(doc);
-
-        faultArray = faultArray.concat(faultTrans);
-
-        // falls Fehler gefunden werden --> Fehler anzeigen und Programm abberchen
-        if (markFaults(faultArray, doc)) {
-            vscode.window.showErrorMessage('Fehler >> siehe Menü -> Anzeigen -> Probleme');
-            return;
-        }
-
-        onlyFromat(doc, editor);
+        processDocument(onlyFromat);
     });
+
     context.subscriptions.push(cleanupDisposable);
     context.subscriptions.push(onlyFormatDisposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
+
+
+function processDocument(process: (doc: vscode.TextDocument, editor: vscode.TextEditor) => void) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor){
+        return;
+    }
+    const doc = editor.document;
+    if (isIBNArc(doc)) {
+        vscode.window.showErrorMessage('Fehler >> siehe Menü -> Anzeigen -> Probleme');
+        return;
+    }
+    let faultArray = openClose(doc).concat(openCloseTrans(doc));
+    if (markFaults(faultArray, doc)) {
+        vscode.window.showErrorMessage('Fehler >> siehe Menü -> Anzeigen -> Probleme');
+        return;
+    }
+    process(doc, editor);
+}
 
